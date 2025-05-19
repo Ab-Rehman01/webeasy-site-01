97a5a09bf08270413eaa628eb397076e1310f83a
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-05-19 16:25:10 +0500

    main

--- src/app/templates/[slug]/[templateId]/page.tsx
+++ src/app/templates/[slug]/[templateId]/page.tsx
@@ -36,12 +36,13 @@ export default function TemplateDetailPage({ params }: Props) {
 
 
       <p className="mb-4 text-green-700">
+         <h1 className="text-xl font-semibold mb-2 text-black">Key Features:</h1>
         {template.description || "This is a modern and responsive template perfect for businesses."}
       </p>
       {template.features && template.features.length > 0 && (
         <div className="mb-6">
-          <h3 className="text-xl font-semibold mb-2 text-black">Key Features:</h3>
-          <ul className="list-disc pl-6 space-y-1 text-gray-700 text-sm">
+          <h2 className="text-xl font-semibold mb-2 text-black">Key Features:</h2>
+          <ul className="list-disc pl-6 space-y-1 text-gray-800 text-sm">
             {template.features.map((feature, index) => (
               <li key={index}>{feature}</li>
             ))}

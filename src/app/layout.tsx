f2b85c0dda45635a203aa751900f9f258bd6fdab
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-06-14 09:05:49 +0500

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,9 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en"><head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className="relative text-white">
         {/* Background Image with Overlay */}
         <div className="fixed inset-0 z-[-1]">

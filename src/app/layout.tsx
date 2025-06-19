7b5e2ff2162eecd7223dd8871a42602ab7bac9f1
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-06-19 20:30:30 +0500

    main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+<link rel="manifest" href="/images/favicon/site.webmanifest" />
 
         {/* Social Meta Tags */}
         <meta property="og:title" content="Webeasy-Tech | Web Development & Marketing Services" />

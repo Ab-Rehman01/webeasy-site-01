e79b5a51264e11a77c122b24246c2bc9f1faaf91
Ab-Rehman01 <ab-rehman004@outlook.com>
2026-05-03 23:25:33 +0500

    update

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -81,9 +81,18 @@ export default function Services() {
           className="text-4xl font-bold mb-4"
         >
           Our Services
+          <br>
           <h1>
             Best Web Development & SEO Agency in Pakistan
           </h1>
+          <h1 className="text-4xl font-bold">
+        Best Web Development & SEO Agency in Pakistan
+      </h1>
+
+      <p>
+        We build high-converting websites, Shopify stores, and SEO strategies.
+      </p>
+          </br>
         </motion.h2>
         <p className="text-gray-400 text-lg leading-relaxed">
           We offer a wide range of digital solutions tailored to help your business grow online — from sleek websites to

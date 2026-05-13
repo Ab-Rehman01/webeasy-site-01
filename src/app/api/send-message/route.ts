599ebe3f1339f1e983900c7c082e026334abd9a1
Ab-Rehman01 <ab-rehman004@outlook.com>
2026-05-14 00:09:42 +0500

    toiken

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
       {
         method: "POST",
         headers: {
-          Authorization: `EAAV18Sy2fBoBRdFBWqQGiIlR1ZCnWAbRKLCXogQhl8LX8eF42WRmZB1XKerYU1nZAeyNYvLMZCbjfIdhxTkzhI2qFZAhBn3WaZBZAaCiDuWlLsQ2cd8TOejZCiUpIfSZCN3QOvK79A6tfZAIpmXG3iaduvchKt7nVWFF6MBzX5qhw7vhJSIGuzZCXCqIZAmB5rNnYM8S6OwKlZB85XCZCYnigNqiqqpE7ZCq7YCde0ZCJVc8xFKvbR699zZCDdZBcKgbqmAW5kcWs8XShHLhRJ0mndXCFmDGLswwZDZD`,
+          Authorization: `Bearer EAAV18Sy2fBoBRdFBWqQGiIlR1ZCnWAbRKLCXogQhl8LX8eF42WRmZB1XKerYU1nZAeyNYvLMZCbjfIdhxTkzhI2qFZAhBn3WaZBZAaCiDuWlLsQ2cd8TOejZCiUpIfSZCN3QOvK79A6tfZAIpmXG3iaduvchKt7nVWFF6MBzX5qhw7vhJSIGuzZCXCqIZAmB5rNnYM8S6OwKlZB85XCZCYnigNqiqqpE7ZCq7YCde0ZCJVc8xFKvbR699zZCDdZBcKgbqmAW5kcWs8XShHLhRJ0mndXCFmDGLswwZDZD`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
@@ -25,12 +25,24 @@ export async function POST(req: NextRequest) {
 
     const data = await response.json();
 
+    if (!response.ok) {
+      return NextResponse.json({
+        success: false,
+        error: data,
+      });
+    }
+
     return NextResponse.json({
       success: true,
       data,
       message: "Message Sent Successfully",
     });
   } catch (error) {
-  console.error("Something went wrong:", error);
-}
+    console.error("Something went wrong:", error);
+
+    return NextResponse.json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
 }
